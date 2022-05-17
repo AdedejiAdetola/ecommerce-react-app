5ef329636af8bd576cbf6af317650aa424d8bdcb
unknown <adetolaadedeji0@gmail.com>
2022-05-17 15:01:48 +0100

    Switch and route issue not fixed

--- clothing-app-react/src/App.js
+++ clothing-app-react/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/ShopPage/ShopPage';
 
 function App() {
   return (
     <div>
-      <ShopPage />
+      <HomePage/>
+      <ShopPage/>
     </div>
   );
 }
