05d3865912c513e995eea17083423ae981b2b1da
unknown <adetolaadedeji0@gmail.com>
2022-06-27 01:45:53 +0100

    Add header component to all pages

--- clothing-app-react/src/App.js
+++ clothing-app-react/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/ShopPage/ShopPage';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <Router>
          <div>
+           <Header />
            <Switch>
               <Route exact path="/">
                 <HomePage/>
