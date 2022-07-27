41985757ffca35e2c660b138b34c7d038e9cabfa
unknown <adetolaadedeji0@gmail.com>
2022-07-27 19:06:43 +0100

    Redux

---
+++ clothing-app-react/src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+//import { configureStore } from '@reduxjs/toolkit';
+// configurestore used in place of createstore.
+import logger from 'redux-logger';
+
+import rootReducer from "./root-reducer";
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
