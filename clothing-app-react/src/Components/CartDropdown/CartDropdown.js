7652e3079c15ac9e9e2854b01b1218c242151b19
unknown <adetolaadedeji0@gmail.com>
2022-09-14 16:10:28 +0100

    Cart dropdown accepts cart items

--- clothing-app-react/src/Components/CartDropdown/CartDropdown.js
+++ clothing-app-react/src/Components/CartDropdown/CartDropdown.js
@@ -1,14 +1,27 @@
 import './CartDropdown.scss';
 
 import CustomButton from '../CustomButton/CustomButton';
-const CartDropdown = () => {
+
+import CartItem from '../CartItem/CartItemComponent';
+
+import { connect } from 'react-redux';
+const CartDropdown = ({ cartItems }) => {
     return ( 
         <div className='cart-dropdown'>
-            <p>wagwan</p>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                }
+            </div> 
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
      );
 }
+
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    cartItems
+});
  
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
