890f850b42ee8f0ec7d5bb8f6e110378796e83e7
unknown <adetolaadedeji0@gmail.com>
2022-09-28 02:37:24 +0100

    Increase and decrease checkout item

--- clothing-app-react/src/redux/cart/cart.utils.js
+++ clothing-app-react/src/redux/cart/cart.utils.js
@@ -7,8 +7,24 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
         return cartItems.map(
             cartItem => cartItem.id === cartItemsToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1} :
             cartItem
-        )
+        );
     }
 
-    return [...cartItems, { ...cartItemsToAdd, quantity: 1}]
+    return [...cartItems, { ...cartItemsToAdd, quantity: 1}];
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id ? {
+            ...cartItem, quantity: cartItem.quantity - 1
+        } : cartItem
+    )
 }
